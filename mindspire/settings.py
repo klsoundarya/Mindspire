2448d77c9fc7e0a6410fe6ae927e60e5e176207f
klsoundarya <k.soundarya7@gmail.com>
2024-12-03 16:46:37 +0000

    install django allauth

--- mindspire/settings.py
+++ mindspire/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b2j5yw=hb#^%d7_rdbon)4@hh@irvxcnj$&-sec_(_cx%*vp2z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
